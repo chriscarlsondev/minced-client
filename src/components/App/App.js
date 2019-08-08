e4b949ac36e2d6c7018be59018a6903335f63c6d
Chris Carlson <cecarlson@gmail.com>
2019-08-07 22:04:49 -0500

    coded app.js and added placeholders for routes and components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,81 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
+import LandingPage from '../../routes/LandingPage/LandingPage'
+import HomePage from '../../routes/HomePage/HomePage'
+import AddNewRecipePage from '../../routes/AddNewRecipePage/AddNewRecipePage'
+import ViewRecipePage from '../../routes/ViewRecipePage/ViewRecipePage'
+import { Route, Switch } from 'react-router-dom'
+import MincedContext from '../../MuchToDoContext';
+import './App.css'
+import config from '../../config';
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  state = {
+    Recipes: [],
+  }
+
+  setRasks = recipes => {
+    this.setState({
+      Recipes
+    })
+  }
+
+  getAllRecipes = () => {
+  }
+
+  componentDidMount() {
+    this.getAllRecipes();
+  }
+
+  // Add Recipe
+  handleAddNewRecipe = (recipe) => {
+
+  }
+
+  // Delete Recipe
+  handleDeleteRecipe = (recipeID) => {
+
+  }
+
+  render() {
+    const contextValue = {
+      Recipes: this.state.Recipes,
+      handleAddNewRecipe: this.handleAddNewRecipe,
+      handleAddCategory: this.handleAddCategory,
+      handleDeleteRecipe: this.handleDeleteRecipe,
+    }
+    return (
+      <MincedContext.Provider value={contextValue}>
+        <Header />
+        <main role="main">
+          <Switch>
+            <Route
+              exact
+              path={'/'}
+              component={LandingPage}
+            />
+            <Route
+              exact
+              path={'/home'}
+              component={HomePage}
+            />
+            <Route
+              exact
+              path={'/add'}
+              component={AddRecipePage}
+            />
+            <Route
+              exact
+              path={'/view'}
+              component={ViewRecipePage}
+            />
+          </Switch>
+        </main>
+        <Footer />
+      </MincedContext.Provider>
+    )
+  }
 }
 
 export default App;
