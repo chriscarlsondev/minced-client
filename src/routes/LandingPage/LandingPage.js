18e71bd9d801340ca6abb1aefa3e366232bc76e0
Chris Carlson <cecarlson@gmail.com>
2019-08-12 16:21:15 -0500

    added screenshots to landing page

--- src/routes/LandingPage/LandingPage.js
+++ src/routes/LandingPage/LandingPage.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import LinkButton from '../../components/LinkButton/LinkButton'
-
+import Screenshot1 from './screenshot-1.png'
+import Screenshot2 from './screenshot-2.png'
+import Screenshot3 from './screenshot-3.png'
 export default class LandingPage extends Component {
   render() {
       return <>
@@ -8,20 +10,20 @@ export default class LandingPage extends Component {
         <header>
             <h2>Easily add new recipes</h2>
         </header>
-        <p>[<em>placeholder for screenshot of new recipe interface</em>]</p>
+        <img src={Screenshot1} className="screenshot" alt="Screenshot of Add recipe page" />
         </section>
         <section className="landing-page-section">
         <header>
             <h2>Include as much helpful information as you'd like</h2>
         </header>
-        <p>[<em>placeholder for screenshot of adding a recipe</em>]</p>
+        <img src={Screenshot2} className="screenshot" alt="Screenshot of detailed recipe information" />
         <p>Each recipe can include a description, time to prepare, time to cook, list of ingredients, step-by-step instructions and any additional notes</p>
         </section>
         <section className="landing-page-section">
         <header>
             <h2>Delete recipes</h2>
         </header>
-        <p>[<em>placeholder for screenshot of deleting a recipe</em>]</p>
+        <img src={Screenshot3} className="screenshot" alt="Screenshot of ability to delete a recipe" />
         <p>Did things not pan out? Easily delete a recipe from your collection</p>
         </section>
         <section className="landing-page-section">
