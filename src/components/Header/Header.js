013a37afd66fecdd54ddaab3c54a2cc79fa6fc5c
Chris Carlson <cecarlson@gmail.com>
2019-08-12 00:12:51 -0500

    added nav menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,10 +4,15 @@ import { Link } from 'react-router-dom'
 export default class Header extends Component {
     render() {
         return (
+            <>
+            <nav>
+            <Link to='/home'>Home</Link> &bull; <Link to='/add'>Add Recipe</Link> &bull; <Link to="/">About</Link>
+            </nav>
             <header role="banner">
             <h1><Link to="/home">Minced</Link></h1>
             <h2>All of your recipes saved and organized in one place.</h2>
-          </header>
+            </header>
+            </>
         )
     }
 }
