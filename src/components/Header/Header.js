5c9d63c69796833ebb37593cfc8147b1535259e2
Chris Carlson <cecarlson@gmail.com>
2019-08-11 23:21:00 -0500

    updated logo link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ export default class Header extends Component {
     render() {
         return (
             <header role="banner">
-            <h1><Link to="/">Minced</Link></h1>
+            <h1><Link to="/home">Minced</Link></h1>
             <h2>All of your recipes saved and organized in one place.</h2>
           </header>
         )
